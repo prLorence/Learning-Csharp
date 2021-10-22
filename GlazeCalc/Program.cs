7c214a61823d610e248b3066bb7172a60d5e7e30
prLorence <paulorencereyes@gmail.com>
2021-10-22 12:41:01 +0800

    Added maximum and minimum width conditonals for the window.

--- GlazeCalc/Program.cs
+++ GlazeCalc/Program.cs
@@ -7,10 +7,31 @@ namespace GlazeCalc
         static void Main(string[] args)
         {
             double width, height, woodLength, glassArea;
+
+            const double MAX_WIDTH = 5.0;
+            const double MIN_WIDTH = 0.5;
+            const double MAX_HEIGHT = 3.0;
+            const double MIN_HEIGHT = 0.75;
+
             string widthString, heightString;
 
+            Console.Write("Give the width of the window: ");
             widthString = Console.ReadLine();
             width = double.Parse(widthString);
+
+            if (width < MIN_WIDTH)
+            {
+                Console.WriteLine("Width is too small just like your pee pee. \n \n");
+                Console.WriteLine("Using minimum");
+                width = MIN_WIDTH;
+            }
+
+            if (width > MAX_WIDTH)
+            {
+                Console.WriteLine("Width is too large, it won't fit. \n \n");
+                Console.WriteLine("Using maximum");
+                width = MAX_WIDTH;
+            } 
 
             heightString = Console.ReadLine();
             height = double.Parse(heightString);
