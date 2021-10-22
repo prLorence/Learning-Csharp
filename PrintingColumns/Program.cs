da0225284d4ed23843bae4d9ce00a03c9e4d72b4
prLorence <paulorencereyes@gmail.com>
2021-10-22 14:15:06 +0800

    Written the proper syntax for printing columns from left to right and right to left. Also implemented how standard numbers are written.

--- PrintingColumns/Program.cs
+++ PrintingColumns/Program.cs
@@ -6,7 +6,23 @@ namespace PrintingColumns
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // formatting standard for thousand numbers
+            int i = 150;
+            double f = 1234.56789;
+            Console.WriteLine("Printing standard format");
+            Console.WriteLine("i: {0:#, ##0} f:{1:##,##0.00} \n", i, f);
+
+            // printing columns
+            Console.WriteLine("Printing columns (appearing to the right side)");
+            Console.WriteLine("i: {0,10:0} f: {1,15:0.00}", i, f);
+            Console.WriteLine("i: {0,10:0} f: {1,15:0.00} \n", 0, 0);
+
+            Console.WriteLine("Printing columns (appearing to the left side)");
+            Console.WriteLine("i: {0,-10:0} f: {1,-15:0.00}", i, f);
+            Console.WriteLine("i: {0,-10:0} f: {1,-15:0.00}", 0, 0);
+
+
+            Console.ReadLine();
         }
     }
 }
